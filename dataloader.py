1a69122566b1b57e09810394bba7fbf006240728
phattran <phat.tranhoang@cyberlogitec.com>
2021-02-23 09:23:34 +0700

    ok

--- dataloader.py
+++ dataloader.py
@@ -13,6 +13,7 @@ class DatasetRetriever(Dataset):
         image_ids,
         transforms=None,
         test=False,
+        image_size=640,
         root_path='/content',
         fold_dict={}
     ):
@@ -22,17 +23,20 @@ class DatasetRetriever(Dataset):
         self.marking = marking
         self.transforms = transforms
         self.test = test
+        self.image_size = image_size
         self.root_path = root_path
         self.fold_dict = fold_dict
+        self.mosaic_border = [-image_size // 2, -image_size // 2]
 
     def __getitem__(self, index: int):
         image_id = self.image_ids[index]
         
-        if self.test or random.random() > 0.5:
+        if self.test or random.random() > 5:
             image, boxes, labels = self.load_image_and_boxes(index)
         else:
             image, boxes, labels = self.load_mosaic(index)
         
+        print(labels)
         target = {}
         target['boxes'] = boxes
         target['labels'] = torch.tensor(labels)
@@ -68,24 +72,25 @@ class DatasetRetriever(Dataset):
         labels = records['class_id'].values
         return image, boxes, labels
 
-    def load_mosaic(self, index, imsize=1024):
+    def load_mosaic(self, index):
         """ 
         This implementation of cutmix author:  https://www.kaggle.com/nvnnghia 
         Refactoring and adaptation: https://www.kaggle.com/shonenkov
         """
-        w, h = imsize, imsize
-        s = imsize // 2
+        imsize = self.image_size
+        s = imsize
     
-        xc, yc = [int(random.uniform(imsize * 0.25, imsize * 0.75)) for _ in range(2)]  # center x, y
+        yc, xc = [int(random.uniform(-x, 2 * s + x)) for x in self.mosaic_border]
         indexes = [index] + [random.randint(0, self.image_ids.shape[0] - 1) for _ in range(3)]
 
-        result_image = np.full((imsize, imsize, 3), 1, dtype=np.float32)
         result_boxes = []
         result_labels = []
 
         for i, index in enumerate(indexes):
             image, boxes, labels = self.load_image_and_boxes(index)
+            h, w, _ = image.shape
             if i == 0:
+                result_image = np.full((s * 2, s * 2, 3), 1, dtype=np.float32)
                 x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
                 x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
             elif i == 1:  # top right
@@ -93,7 +98,7 @@ class DatasetRetriever(Dataset):
                 x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
             elif i == 2:  # bottom left
                 x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)
-                x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, max(xc, w), min(y2a - y1a, h)
+                x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, w, min(y2a - y1a, h)
             elif i == 3:  # bottom right
                 x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
                 x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
@@ -111,8 +116,11 @@ class DatasetRetriever(Dataset):
             result_labels.append(labels)
 
         result_boxes = np.concatenate(result_boxes, 0)
+        result_labels = np.concatenate(result_labels, 0)
         np.clip(result_boxes[:, 0:], 0, 2 * s, out=result_boxes[:, 0:])
         result_boxes = result_boxes.astype(np.int32)
-        result_boxes = result_boxes[np.where((result_boxes[:,2]-result_boxes[:,0])*(result_boxes[:,3]-result_boxes[:,1]) > 0)]
+        valid_indices = np.where((result_boxes[:,2]-result_boxes[:,0])*(result_boxes[:,3]-result_boxes[:,1]) > 0)
+        result_boxes = result_boxes[valid_indices]
+        result_labels = result_labels[valid_indices]
 
         return result_image, result_boxes, result_labels
